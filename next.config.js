f3c66792516cad05cc8b954d9e53fa6d4896eacc
Thomas <tbeckman1234@gmail.com>
2023-08-24 16:33:20 -0600

    Explicitly allow request from origin from app url

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
           source: "/api/:path*",
           headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "https://firebasestorage.googleapis.com/v0/b/nextfire-app-5bdbd.appspot.com/" },
+            { key: "Access-Control-Allow-Origin", value: "https://next-fire-woad.vercel.app" },
             { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
             { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
           ]
